b16376dc12758bd4cf6167c9e8d6d8a12d9d9681
Ted Steen <ted.steen@gmail.com>
2024-11-22 22:34:01 +0000

    break out ball to own file

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 use core::borrow::BorrowMut;
 
+use ball::{Ball, BallUpdateResult};
 use dot_matrix::DotMatrix;
 use embassy_executor::Spawner;
 use embassy_sync::{blocking_mutex::raw::CriticalSectionRawMutex, mutex::Mutex};
@@ -15,105 +16,13 @@ use esp_hal::{
 };
 use esp_println::logger::init_logger_from_env;
 use log::info;
-use pad::{PadAliveState, PadPosition, PadState, PadUpdateResult};
+use pad::{PadAliveState, PadState, PadUpdateResult};
 
+mod ball;
 mod dot_matrix;
 mod pad;
 
 static DOT_MATRIX: Mutex<CriticalSectionRawMutex, Option<DotMatrix<'_>>> = Mutex::new(None);
-
-struct BallUpdateResult {
-    x: u8,
-    y: u8,
-}
-
-#[derive(Clone)]
-struct Ball {
-    x: f32,
-    y: f32,
-    x_speed: f32,
-    y_speed: f32,
-}
-impl Ball {
-    const fn new(initial_x: u8, initial_y: u8) -> Self {
-        Self {
-            x: initial_x as f32,
-            y: initial_y as f32,
-            x_speed: 0.0054,
-            y_speed: -0.004,
-        }
-    }
-
-    fn update(&mut self, pad: &mut PadState, delta_time_ms: u64) -> BallUpdateResult {
-        let mut pad_hit = false;
-        if let PadState::Alive {
-            position,
-            state: PadAliveState::Normal,
-            ..
-        } = &pad
-        {
-            // Check X collision
-            if self.x_speed < 0.0 {
-                let min_x = if matches!(position, PadPosition::Left) {
-                    1.5
-                } else {
-                    0.5
-                };
-                if self.x < min_x {
-                    pad_hit = matches!(position, PadPosition::Left);
-                    self.x = min_x;
-                    self.x_speed *= -1.0;
-                }
-            } else {
-                let max_x = if matches!(position, PadPosition::Right) {
-                    6.5
-                } else {
-                    7.5
-                };
-
-                if self.x >= max_x {
-                    pad_hit = matches!(position, PadPosition::Right);
-                    self.x = max_x;
-                    self.x_speed *= -1.0;
-                }
-            }
-
-            // Check Y collision
-            if self.y_speed < 0.0 {
-                let min_y = if matches!(position, PadPosition::Top) {
-                    1.5
-                } else {
-                    0.5
-                };
-                if self.y < min_y {
-                    pad_hit = matches!(position, PadPosition::Top);
-                    self.y = min_y;
-                    self.y_speed *= -1.0;
-                }
-            } else {
-                let max_y = if matches!(position, PadPosition::Bottom) {
-                    6.5
-                } else {
-                    7.5
-                };
-                if self.y >= max_y {
-                    pad_hit = matches!(position, PadPosition::Bottom);
-                    self.y = max_y;
-                    self.y_speed *= -1.0;
-                }
-            }
-            if pad_hit {
-                pad.take_damage();
-            }
-            self.x += self.x_speed * delta_time_ms as f32;
-            self.y += self.y_speed * delta_time_ms as f32;
-        }
-        BallUpdateResult {
-            x: self.x as u8,
-            y: self.y as u8,
-        }
-    }
-}
 
 static GAME_STATE: Mutex<CriticalSectionRawMutex, GameState> = Mutex::new(GameState::new());
 struct GameState {
